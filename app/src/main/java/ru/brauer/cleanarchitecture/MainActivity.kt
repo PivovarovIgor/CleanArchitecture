4087a290392b2525b74f8e51c26e67306072f9fe
PivIgor <brauer.igor@gmail.com>
2021-11-27 02:07:01 +0300

    Intermediate commit.

--- app/src/main/java/ru/brauer/cleanarchitecture/MainActivity.kt
+++ app/src/main/java/ru/brauer/cleanarchitecture/MainActivity.kt
@@ -1,11 +1,233 @@
 package ru.brauer.cleanarchitecture
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableObserver
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+
+class MainAdapter(
+    private var onListItemClickListener: OnListItemClickListener,
+    private var data: List<DataModel>
+) : RecyclerView.Adapter<MainAdapter.RecyclerItemViewHolder>() {
+
+    fun setData(data: List<DataModel>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerItemViewHolder {
+        return RecyclerItemViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.activity_main_recyclerview_item, parent, false) as View
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerItemViewHolder, position: Int) {
+        holder.bind(data.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class RecyclerItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(data: DataModel) {
+            if (layoutPosition != RecyclerView.NO_POSITION) {
+                itemView.findViewById<TextView>(R.id.header_textview_recycler_item).text = data.text
+                itemView.findViewById<TextView>(R.id.description_textview_recycler_item).text =
+                    data.meanings?.get(0)?.translation?.translation
+                itemView.setOnClickListener { openInNewWindow(data) }
+            }
+        }
+    }
+
+    private fun openInNewWindow(listItemData: DataModel) {
+        onListItemClickListener.onItemClick(listItemData)
+    }
+
+    interface OnListItemClickListener {
+        fun onItemClick(data: DataModel)
+    }
+}
+
+class MainPresenterImpl<T : AppState, V : ru.brauer.cleanarchitecture.View>(
+    private val interactor: MainInteractor = MainInteractor(
+        RepositoryImplementation(DataSourceRemote()),
+        RepositoryImplementation(DataSourceLocal())
+    ),
+    protected val compositeDisposable: CompositeDisposable = CompositeDisposable(),
+    protected val schedulerProvider: SchedulerProvider = SchedulerProvider()
+) : Presenter<T, V> {
+    private var currentView: V? = null
+    override fun attachView(view: V) {
+        currentView = view
+    }
+
+    override fun detachView(view: V) {
+        compositeDisposable.clear()
+        if (view == currentView) {
+            currentView = null
+        }
+    }
+
+    override fun getData(word: String, isOnline: Boolean) {
+        compositeDisposable.add(
+            interactor.getData(word, isOnline)
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+                .doOnSubscribe { currentView?.renderData(AppState.Loading(null)) }
+                .subscribeWith(getObserver())
+        )
+    }
+
+    private fun getObserver(): DisposableObserver<AppState> {
+        return object : DisposableObserver<AppState>() {
+            override fun onNext(appState: AppState) {
+                currentView?.renderData(appState)
+            }
+
+            override fun onError(e: Throwable) {
+                currentView?.renderData(AppState.Error(e))
+            }
+
+            override fun onComplete() {
+
+            }
+        }
+    }
+}
+
+class MainInteractor(
+    private val remoteRepository: Repository<List<DataModel>>,
+    private val localRepository: Repository<List<DataModel>>
+) : Interactor<AppState> {
+    override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
+        return if (fromRemoteSource) {
+            remoteRepository.getData(word).map { AppState.Success(it) }
+        } else {
+            localRepository.getData(word).map { AppState.Success(it) }
+        }
+    }
+}
+
+class RepositoryImplementation(private val dataSource: DataSource<List<DataModel>>) :
+    Repository<List<DataModel>> {
+
+    override fun getData(word: String): Observable<List<DataModel>> {
+        return dataSource.getData(word)
+    }
+}
+
+class DataSourceRemote(private val remoteProvider: RetrofitImplimentation = RetrofitImplimentation()) :
+    DataSource<List<DataModel>> {
+    override fun getData(word: String): Observable<List<DataModel>> = remoteProvider.getData(word)
+}
+
+class DataSourceLocal(private val localProvider: RoomDataBaseImplementation = RoomDataBaseImplementation()) :
+    DataSource<List<DataModel>> {
+    override fun getData(word: String): Observable<List<DataModel>> =
+        localProvider.getData(word)
+}
+
+class RoomDataBaseImplementation : DataSource<List<DataModel>> {
+    override fun getData(word: String): Observable<List<DataModel>> {
+        TODO("Not yet implemented")
+    }
+}
+
+class RetrofitImplimentation : DataSource<List<DataModel>> {
+    override fun getData(word: String): Observable<List<DataModel>> {
+        return getService(BaseInterceptor.interceptor).search(word)
+    }
+
+    private fun getService(interceptor: Interceptor): ApiService {
+        return createRetrofit(interceptor).create(ApiService::class.java)
+    }
+
+    private fun createRetrofit(interceptor: Interceptor): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL_LOCATION)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(createOkHttpClient(interceptor))
+            .build()
+    }
+
+    private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor)
+        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        return httpClient.build()
+    }
+
+    companion object {
+        private const val BASE_URL_LOCATION = "https://dictionary.skyeng.ru/api/public/v1/"
+    }
+
+}
+
+class BaseInterceptor private constructor() : Interceptor{
+
+    private var responseCode: Int = 0
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(chain.request())
+        responseCode = response.code()
+        return response
+    }
+
+    fun getResponseCode(): ServerResponseStatusCode {
+        var statusCode = ServerResponseStatusCode.UNDEFINED_ERROR
+        when (responseCode / 100) {
+            1 -> statusCode = ServerResponseStatusCode.INFO
+            2 -> statusCode = ServerResponseStatusCode.SUCCESS
+            3 -> statusCode = ServerResponseStatusCode.REDIRECTION
+            4 -> statusCode = ServerResponseStatusCode.CLIENT_ERROR
+            5 -> statusCode = ServerResponseStatusCode.SERVER_ERROR
+        }
+        return statusCode
+    }
+
+    enum class ServerResponseStatusCode {
+        INFO,
+        SUCCESS,
+        REDIRECTION,
+        CLIENT_ERROR,
+        SERVER_ERROR,
+        UNDEFINED_ERROR
+    }
+
+    companion object {
+        val interceptor: BaseInterceptor
+        get() = BaseInterceptor()
+    }
+}
+
+interface ApiService {
+    @GET("words/search")
+    fun search(@Query("search") wordToSearch: String): Observable<List<DataModel>>
 }
---
+++ app/src/main/java/ru/brauer/cleanarchitecture/View.kt
@@ -0,0 +1,79 @@
+package ru.brauer.cleanarchitecture
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.annotations.SerializedName
+import io.reactivex.rxjava3.core.Observable
+
+interface View {
+    fun renderData(appState: AppState)
+}
+
+interface Presenter<T : AppState, V : View> {
+
+    fun attachView(view: V)
+
+    fun detachView(view: V)
+
+    fun getData(word: String, isOnline: Boolean)
+}
+
+interface Interactor<T> {
+
+    fun getData(word: String, fromRemoteSource: Boolean): Observable<T>
+}
+
+interface Repository<T> {
+
+    fun getData(word: String): Observable<T>
+}
+
+interface DataSource<T> {
+
+    fun getData(word: String): Observable<T>
+}
+
+sealed class AppState {
+
+    data class Success(val data: List<DataModel>?) : AppState()
+
+    data class Error(val error: Throwable) : AppState()
+
+    data class Loading(val progress: Int?) : AppState()
+}
+
+class DataModel(
+    @field:SerializedName("text") val text: String?,
+    @field:SerializedName("meanings") val meanings: List<Meanings>?
+)
+
+class Meanings(
+    @field:SerializedName("translation") val translation: Translation?,
+    @field:SerializedName("imageUrl") val imageUrl: String?
+)
+
+class Translation(@field:SerializedName("text") val translation: String?)
+
+abstract class BaseActivity<T : AppState> : AppCompatActivity(), View {
+
+    protected lateinit var presenter: Presenter<T, View>
+
+    protected abstract fun createPresenter(): Presenter<T, View>
+
+    abstract override fun renderData(appState: AppState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = createPresenter()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachView(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.detachView(this)
+    }
+}
