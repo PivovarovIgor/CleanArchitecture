57d3ea333f726f137f39a75e08aadb3790a2e73f
PivIgor <brauer.igor@gmail.com>
2021-11-30 22:37:21 +0300

    Removed spaces between lines in interfaces.

--- app/src/main/java/ru/brauer/cleanarchitecture/presenter/Presenter.kt
+++ app/src/main/java/ru/brauer/cleanarchitecture/presenter/Presenter.kt
@@ -5,6 +5,5 @@ import ru.brauer.cleanarchitecture.view.base.View
 interface Presenter<T , V : View<T>> {
 
     fun attachView(view: V)
-
     fun detachView(view: V)
 }
--- app/src/main/java/ru/brauer/cleanarchitecture/rx/ISchedulerProvider.kt
+++ app/src/main/java/ru/brauer/cleanarchitecture/rx/ISchedulerProvider.kt
@@ -6,6 +6,5 @@ import io.reactivex.rxjava3.core.Scheduler
 interface ISchedulerProvider {
 
     fun ui(): Scheduler
-
     fun io(): Scheduler
 }
